c9b11c14d546d1d03739714e75e2431f2c5b5ef7
Andrey Drozdov <dividedby0zero@gmail.com>
2019-05-11 18:32:02 +0300

    Copying static assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
+const path = require('path');
 const gulp = require('gulp');
 const hb = require('gulp-hb');
-const path = require('path');
+const copy = require('gulp-copy');
 const browserSync = require('browser-sync').create();
 
 const srcBase = path.resolve(__dirname, 'src');
 const distBase = path.resolve(__dirname, 'dist');
 
+const staticSrc = [
+    path.resolve(srcBase, 'assets/*'),
+    path.resolve(srcBase, 'script/*'),
+    path.resolve(srcBase, 'style/*'),
+];
+
 function runBrowserSync(cb) {
     browserSync.init({
         server: {
-            baseDir: distBase,
-        }
+            baseDir: path.resolve(distBase),
+        },
     });
 
     cb();
@@ -18,7 +25,7 @@ function runBrowserSync(cb) {
 
 function reloadBrowserSync(cb) {
     gulp.watch(
-        path.resolve(distBase, '*'),
+        path.resolve(distBase),
         browserSync.reload
     );
 
@@ -42,14 +49,37 @@ function watchMarkup(cb) {
     cb();
 }
 
+function copyStatic(cb) {
+    gulp.src(staticSrc)
+        .pipe(copy(path.resolve(distBase), { prefix: 1 }))
+        // .pipe(gulp.dest(path.resolve(distBase)));
+
+    cb();
+}
+
+function watchStatic(cb) {
+    gulp.watch(
+        staticSrc,
+        copyStatic
+    );
+
+    cb();
+}
+
 gulp.task('start', gulp.series(
     runBrowserSync,
     reloadBrowserSync,
     gulp.parallel(
-        watchMarkup
+        buildMarkup,
+        copyStatic,
+    ),
+    gulp.parallel(
+        watchMarkup,
+        watchStatic
     )
 ));
 
 gulp.task('build', gulp.parallel(
-    buildMarkup
+    buildMarkup,
+    copyStatic
 ));
