e4b3cef0771258b9fb22d50bfa4bb1bd36f81685
Andrey Drozdov <dividedby0zero@gmail.com>
2019-05-26 11:43:03 +0300

    Updates

---
+++ script/age-calc.js
@@ -0,0 +1,30 @@
+const phases = [
+    {
+        length: 12,
+        rate: 15
+    },
+    {
+        length: 12,
+        rate: 9
+    },
+    {
+        length: 12 * 8,
+        rate: 4,
+    },
+    {
+        length: Infinity,
+        rate: 3,
+    }
+];
+
+export function calcAge(years = 0, months = 0) {
+    let humanAge = (years * 12) + months;
+
+    return phases.reduce((catsAge, { length, rate }) => {
+        const phaseCatsAge = Math.min(humanAge > 0 ? humanAge : 0, length) * rate;
+        
+        humanAge -= length;
+
+        return catsAge + phaseCatsAge;
+    }, 0);
+}
