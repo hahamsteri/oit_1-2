9c0f3e15e8f139b7fb936030503417613dc4ca05
Andrey Drozdov <dividedby0zero@gmail.com>
2019-05-29 00:55:29 +0300

    Конверсия ссылок

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const hb = require('gulp-hb');
 const copy = require('gulp-copy');
 const ghPages = require('gh-pages');
+const cheerio = require('gulp-cheerio');
 const browserSync = require('browser-sync').create();
 
 const srcBase = path.resolve(__dirname, 'src');
@@ -39,10 +40,51 @@ function reloadBrowserSync(cb) {
 function buildMarkup(cb) {
     gulp.src([path.resolve(srcBase, '**/*.html'), `!${path.resolve(srcBase, 'partials/*.html')}`])
         .pipe(hb().partials(path.resolve(srcBase, 'partials/*.html')))
+        .pipe(cheerio({
+            parserOptions: {
+                decodeEntities: false,
+            },
+            run: ($, file, done) => {
+                const relativeness = path.relative(
+                    path.resolve(file.path),
+                    srcBase
+                ).split('/').length - 1;
+
+                const getRelative = src => {
+                    let relPart = '';
+
+                    for (let i = 0; i < relativeness; i++) {
+                        relPart += '../'
+                    };
+
+                    return `${relPart}${[...src].slice(1).join('')}`;
+                }
+
+                $('img, script').each(function () {
+                    const el = $(this);
+                    const src = el.attr('src');
+
+                    if (src && src.startsWith('/')) {
+                        el.attr('src', getRelative(src));
+                    }
+                });
+
+                $('link, a').each(function () {
+                    const el = $(this);
+                    const src = el.attr('href');
+
+                    if (src && src.startsWith('/')) {
+                        el.attr('href', getRelative(src));
+                    }
+                });
+
+                done();
+            }
+        }))
         .pipe(gulp.dest(distBase));
     
     cb();
-};
+}
 
 function watchMarkup(cb) {
     gulp.watch(
